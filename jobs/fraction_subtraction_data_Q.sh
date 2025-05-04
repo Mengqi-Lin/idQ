cd0ff884dea93c092f8cfd2c85034c9958517cc4
Mengqi-Lin <lemonkey@umich.edu>
2025-05-03 22:24:02 -0400

    deleted all unused fns

--- jobs/fraction_subtraction_data_Q.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=fraction_subtraction_data_Q
-#SBATCH --ntasks=1
-#SBATCH --time=30:00:00
-#SBATCH --mem-per-cpu=32G
-#SBATCH --out=../log/fraction_subtraction_data_Q.out
-#SBATCH --error=../log/fraction_subtraction_data_Q.err
-/sw/pkgs/arc/python3.10-anaconda/2023.03/bin/python /home/lemonkey/idQ/py/fraction_subtraction_data_Q.py 
